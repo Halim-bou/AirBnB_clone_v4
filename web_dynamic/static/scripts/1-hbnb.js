cedd5a6b03354be29642ded932c3e03b52ec9fe7
RadouaneAbn <radouaneabn@gmail.com>
2024-04-06 16:56:12 +0000

    Task 2: Full

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  start();
+});
+
+function start () {
+  const amenityIds = [];
+  const amenityNames = [];
+
+  $(document).on('change', 'input[type="checkbox"]', function () {
+    if ($(this).is(':checked')) {
+      amenityIds.push($(this).attr('data-id'));
+      amenityNames.push($(this).attr('data-name'));
+    } else {
+      const indexId = amenityIds.indexOf($(this).attr('data-id'));
+      if (indexId !== -1) {
+        amenityIds.splice(indexId, 1);
+        amenityNames.splice(indexId, 1);
+      }
+    }
+    // console.log(amenityNames);
+    $('.amenities h4').text(amenityNames.join(', '));
+  });
+}
