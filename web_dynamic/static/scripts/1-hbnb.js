a21e0f30b5305de55f444707bd51ff5ca4f5d38f
aelbouaa <hbouaami@gmail.com>
2024-04-06 04:41:04 +0000

    substring the h4 tag for not going out from a line add h4 style to alaways display amenity with no multiple line in popover style change the with from a percent to a pixcels sise

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
   const $amenityIds = [];
+  const $amenityNames = [];
   $(document).on('change', '.checkbox-amenity', function () {
     const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    // console.log(amenityId)
     if ($(this).is(':checked')) {
       $amenityIds.push(amenityId);
+      $amenityNames.push(amenityName);
+      console.log("added" + amenityId)
     } else {
       const index = $amenityIds.indexOf(amenityId);
+      const indexName = $amenityNames.indexOf(amenityName)
       if (index !== -1) {
         $amenityIds.splice(index, 1);
+        $amenityNames.splice(indexName, 1);
       }
+      console.log("removed" + amenityId)
     }
     updateAmenitiesList();
   });
   function updateAmenitiesList () {
-    const amenitiesList = $amenityIds.join(', ');
-    $('#amenities h4').text(amenitiesList);
+    const amenitiesList = $amenityNames.join(', ');
+    var maxLength = 30;
+    console.log(amenitiesList);
+    if (amenitiesList.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    }else {
+      $('.amenities h4').text(amenitiesList.substring(0, maxLength) + '...');
+    }
   }
 });
